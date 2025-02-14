3c7a68b31d9c5075c1227e499ac2d0816562e497
Nikhil Menghani <nikhil@menghani.com>
2025-02-13 22:56:50 -0500

    Fix build.gradle.kts to sign at release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
