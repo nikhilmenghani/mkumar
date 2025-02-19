b7c171fe3a9546eb9064f0dbb9f0fab4455dae09
Nikhil Menghani <nikhil@menghani.com>
2025-02-19 18:26:58 -0500

    Fix up theme warning

--- app/src/main/java/com/mkumar/ui/theme/NikTheme.kt
+++ app/src/main/java/com/mkumar/ui/theme/NikTheme.kt
@@ -10,10 +10,10 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.mkumar.App.Companion.globalClass
 import com.mkumar.data.ThemePreference
 
@@ -81,8 +81,11 @@ fun NikTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = customColorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+            WindowInsetsControllerCompat(window, view).apply {
+                isAppearanceLightStatusBars = !darkTheme
+                isAppearanceLightNavigationBars = !darkTheme
+            }
         }
     }
 
